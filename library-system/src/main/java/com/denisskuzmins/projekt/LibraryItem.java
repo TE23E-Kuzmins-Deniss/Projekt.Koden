d2809556feb01992810d08363717c2e9d17c0254
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2026-05-06 16:03:04 +0200

    Färdig med klasser, ska börja med main.java

--- library-system/src/main/java/com/denisskuzmins/projekt/LibraryItem.java
+++ library-system/src/main/java/com/denisskuzmins/projekt/LibraryItem.java
@@ -1,11 +1,11 @@
 package com.denisskuzmins.projekt;
 
 public class LibraryItem {
-    protected String id;
+    protected int id;
     protected String title;
     protected boolean isAvailable;
 
-    public LibraryItem(String id, String title, boolean isAvailable) {
+    public LibraryItem(int id, String title, boolean isAvailable) {
         this.id = id;
         this.title = title;
         this.isAvailable = isAvailable;
--- library-system/src/main/java/com/denisskuzmins/projekt/Magazine.java
+++ library-system/src/main/java/com/denisskuzmins/projekt/Magazine.java
@@ -1,5 +1,14 @@
 package com.denisskuzmins.projekt;
 
-public class Magazine {
-    
+public class Magazine extends LibraryItem{
+    private int issueNumber;
+    private String category;
+    private int publicationYear;
+
+    public Magazine(int id, String title, int issueNumber, String category, int publicationYear, boolean isAvailable) {
+        super(id, title, isAvailable);
+        this.issueNumber = issueNumber;
+        this.category = category;
+        this.publicationYear = publicationYear;
+    }
 }
